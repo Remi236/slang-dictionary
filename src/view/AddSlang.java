390b058626b124c7595a55d03ed0832f3417c931
Remi236 <daoanhtu236@gmail.com>
2022-06-27 23:23:37 +0700

    add addSlang, Update view

---
+++ src/view/AddSlang.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class AddSlang {
+    private JPanel addPanel;
+    private JTextField txt_word;
+    private JTextField txt_def;
+    private JButton btn_save;
+    private JButton btn_cancel;
+}
---
+++ src/view/UpdateSlang.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class UpdateSlang {
+    private JTextField txt_word;
+    private JTextField txt_def;
+    private JButton btn_save;
+    private JButton btn_cancel;
+    private JPanel updatePanel;
+}
